913522ace90ca9a18045b12293072ff32f37992a
XiaoqiLu <lx2170@cumc.columbia.edu>
2020-04-03 19:20:44 -0400

    created package rl created seeding.py under sub-module utils
    Signed-off-by: XiaoqiLu <lx2170@cumc.columbia.edu>

---
+++ rl/utils/seeding.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def multiple_seeds(seed=None, size=None):
+    """
+    generate multiple seeds from single seed
+    Args:
+        seed: the seed used to generate more seeds
+        size: # of seeds to generate
+
+    Returns:
+        seeds
+    """
+    rng = np.random.RandomState(seed=seed)
+    seeds = rng.tomaxint(size=size)
+    return seeds
