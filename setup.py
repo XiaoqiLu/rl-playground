d226ecbceb9f98c3eb74e654e419acca8282c668
XiaoqiLu <lx2170@cumc.columbia.edu>
2020-05-18 10:37:03 -0400

    sparse_lei.py: fixed error when implementing Lei's paper discrete_bin.py: DiscreteBin Environment (to test on "edge" cases) __init__.py: update DiscreteBin
    Signed-off-by: XiaoqiLu <lx2170@cumc.columbia.edu>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rl',
+    version='1.0',
+    packages=['rl', 'rl.core', 'rl.envs', 'rl.utils', 'rl.agents'],
+    url='',
+    license='',
+    author='XL',
+    author_email='lx2170@cumc.columbia.edu',
+    description=''
+)
