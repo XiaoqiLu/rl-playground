40ef49f30a9f27921b623d6ccb3c73d53fe06efa
XiaoqiLu <lx2170@cumc.columbia.edu>
2020-05-18 10:51:56 -0400

    setup.py
    Signed-off-by: XiaoqiLu <lx2170@cumc.columbia.edu>

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='rl-playground',
+    name='RL Playground',
     version='1.0',
     packages=['rl', 'rl.core', 'rl.envs', 'rl.utils', 'rl.agents'],
-    url='',
+    url='https://github.com/XiaoqiLu/rl-playground',
     license='',
     author='XL',
     author_email='lx2170@cumc.columbia.edu',
-    description=''
+    description='my rl playground'
 )
