51e21e3ad70c258b9f1c1514a03edb29a7af7332
Xiaoqi Lu <lx2170@cumc.columbia.edu>
2020-06-21 23:13:25 -0700

    replace requirements.txt by setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rl-playground',
+    version='1.0',
+    packages=['rl', 'rl.core', 'rl.envs', 'rl.utils', 'rl.agents'],
+    url='https://github.com/XiaoqiLu/rl-playground',
+    license='',
+    author='Xiaoqi Lu',
+    author_email='lx2170@cumc.columbia.edu',
+    description='A general-purpose playground for reinforcement learning'
+)
